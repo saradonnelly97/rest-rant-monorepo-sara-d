9a3d1f376661e143b01902f81d02e837c0a24f4f
saradonnelly97 <sara.donnelly8@gmail.com>
2023-10-13 22:32:49 -0400

    fixed port issues

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -18,7 +18,7 @@ function LoginForm() {
   
   
     async function handleSubmit(e) {
-        const response = await fetch(`http://localhost:5000/authentication/`, {
+        const response = await fetch(`http://localhost:8000/authentication/`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -30,6 +30,7 @@ function LoginForm() {
     
         if (response.status === 200) {
             setCurrentUser(data.user)
+            localStorage.setItem('token', data.token)
             history.push(`/`)
         } else {
             setErrorMessage(data.message)
